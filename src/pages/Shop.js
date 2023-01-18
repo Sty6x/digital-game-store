9af49c63b963765d764ac13870a91e2bf2aa3a7d
Fdiaz <jerylfranz460@gmail.com>
2023-01-18 17:02:32 +0400

    able to fetch and parse api

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,18 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Shop = () => {
+	let [games, setGames] = useState([]);
+	let [loading, setLoading] = useState(true);
+	console.log(loading);
 	async function fetchItems() {
-		const itemsData = await fetch("https://api.skinport.com/v1/items", {
-			mode: "cors",
-		});
-		// const items = await itemsData.json()
-		console.log(itemsData);
+		try {
+			const itemsData = await fetch("https://www.cheapshark.com/api/1.0/deals?storeID=1", {
+				mode: "cors",
+			});
+			const items = await itemsData.json();
+			console.log(items)
+			setGames([...items]);
+			console.log(games)
+			setLoading((loading = false));
+		} catch (err) {
+			console.log(err);
+		}
 	}
 	useEffect(() => {
 		fetchItems();
 	}, []);
 
-	return <div>Shop</div>;
+	const displayGames = games.map((game)=>{
+		return <li>{game.title} </li>
+	}) 
+
+	return (
+		<>
+			<h1>Shop</h1>
+			{!loading ? <> {displayGames}</> : <>loading is true</>}
+		</>
+	);
 };
 
 export default Shop;

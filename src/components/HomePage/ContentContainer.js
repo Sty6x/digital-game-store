2644127f62f96ede087d10aad4949a3994f64e7d
Fdiaz <jerylfranz460@gmail.com>
2023-01-18 23:38:01 +0400

    needed to sort based on metacritic score and take use n games

--- src/components/HomePage/ContentContainer.js
+++ src/components/HomePage/ContentContainer.js
@@ -5,6 +5,7 @@ const ContentContainer = () => {
 	const [gameList, setGameList] = useState([]);
 	let [sortedGameList, setSortedGameList] = useState([]);
 	const [loading, setLoading] = useState(true);
+	const [subLoad, setSubLoad] = useState(true);
 
 	async function fetchGameList() {
 		const FETCHED_GAME_LIST = await fetch(`https://www.cheapshark.com/api/1.0/deals?storeID=1`);
@@ -19,40 +20,38 @@ const ContentContainer = () => {
 
 	useEffect(() => {
 		if (!loading) {
-            console.log('loading done')
-			getSortedGameList(5);
+			console.log("fetch loading done");
+			console.log(gameList);
+			getSortedGameList(10);
 		}
 	}, [loading]);
 
 	const sortedGames = gameList.sort((game1, game2) =>
-		game1.savings < game2.savings ? 1 : game1.savings > game2.savings ? -1 : 0
+		game1.metacriticScore < game2.metacriticScore
+			? 1
+			: game1.metacriticScore > game2.metacriticScore
+			? -1
+			: 0
 	);
 
 	function getSortedGameList(n, arr = sortedGames) {
-		const tmpArr = [];
 		for (let i = 0; i < n; i++) {
-			tmpArr.push(arr[i]);
+			// tmpArr.push(arr[i]);
+			setSortedGameList((prevSorted) => [...prevSorted, arr[i]]);
 		}
-		setSortedGameList(() => {
-			sortedGameList = [...tmpArr];
-			console.log(sortedGameList);
-		});
+		setSubLoad(!subLoad);
 	}
 
-	const DISPLAY_GAMES = sortedGameList.map((game) => {
-		return <li key={game.steamAppID}>{game.title}</li>;
+	useEffect(() => {
+		if (!subLoad) {
+			console.log("subload done");
+			console.log(sortedGameList);
+		}
+	}, [subLoad]);
+
+	const DISPLAY_GAME_LIST = sortedGameList.map((game) => {
+		return <li key={game.steamAppID}> {game.title}</li>;
 	});
-	return (
-		<>
-			{loading ? (
-				<>LOADING CONTENTS...</>
-			) : (
-				<>
-					DONE!
-					{DISPLAY_GAMES}
-				</>
-			)}
-		</>
-	);
+	return <>{subLoad ? <>LOADING CONTENTS...</> : DISPLAY_GAME_LIST}</>;
 };
 export default ContentContainer;

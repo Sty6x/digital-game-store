da488fad0ec5e4d567090825d92331ef8e3c3ad6
Fdiaz <jerylfranz460@gmail.com>
2023-01-18 19:15:09 +0400

    item component able to grab data from api using useParam hook

--- src/pages/RouterSwitch.js
+++ src/pages/RouterSwitch.js
@@ -5,6 +5,7 @@ import NavBar from "../components/Navigation/NavBar";
 import Checkout from "./Checkout";
 import HomePage from "./HomePage";
 import Shop from "./Shop";
+import Item from "../components/Items/Item";
 
 const RouterSwitch = () => {
 	return (
@@ -13,8 +14,9 @@ const RouterSwitch = () => {
 				<NavBar />
 				<Routes>
 					<Route path="/" element={<HomePage />} />
-					<Route path="/shop" element={<Shop/>} />
-					<Route path="/checkout" element={<Checkout/>} />
+					<Route path="/shop" element={<Shop />} />
+					<Route path="/checkout" element={<Checkout />} />
+					<Route path="/shop/:id" element={<Item/>}/>
 				</Routes>
 			</BrowserRouter>
 		</>
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,18 +1,17 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Shop = () => {
 	let [games, setGames] = useState([]);
 	let [loading, setLoading] = useState(true);
-	console.log(loading);
 	async function fetchItems() {
 		try {
 			const itemsData = await fetch("https://www.cheapshark.com/api/1.0/deals?storeID=1", {
 				mode: "cors",
 			});
 			const items = await itemsData.json();
-			console.log(items)
+			console.log(items);
 			setGames([...items]);
-			console.log(games)
 			setLoading((loading = false));
 		} catch (err) {
 			console.log(err);
@@ -22,14 +21,14 @@ const Shop = () => {
 		fetchItems();
 	}, []);
 
-	const displayGames = games.map((game)=>{
-		return <li>{game.title} </li>
-	}) 
+	const displayGames = games.map((game) => {
+		return <li key={game.steamAppID} className="shop-item"> <Link to={`/shop/${game.gameID}`}>{game.title}</Link> </li>;
+	});
 
 	return (
 		<>
 			<h1>Shop</h1>
-			{!loading ? <> {displayGames}</> : <>loading is true</>}
+			<ul className="shop-list">{!loading ? <> {displayGames}</> : <>loading is true</>}</ul>
 		</>
 	);
 };

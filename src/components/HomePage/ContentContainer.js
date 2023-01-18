13baa579ea8b288a45408cd1c9cdc1846c674a17
Fdiaz <jerylfranz460@gmail.com>
2023-01-18 23:46:55 +0400

    styling homepage containers

--- src/components/HomePage/ContentContainer.js
+++ src/components/HomePage/ContentContainer.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import contentStyles from "./Content.module.css";
 
 const ContentContainer = () => {
 	const [gameList, setGameList] = useState([]);
 	let [sortedGameList, setSortedGameList] = useState([]);
 	const [loading, setLoading] = useState(true);
-	const [subLoad, setSubLoad] = useState(true);
+	const [subLoad, setSubLoad] = useState(true); // using subLoad to check if an asynchronous action is done and if so then we can map the newly sorted array
 
 	async function fetchGameList() {
 		const FETCHED_GAME_LIST = await fetch(`https://www.cheapshark.com/api/1.0/deals?storeID=1`);
@@ -22,9 +23,16 @@ const ContentContainer = () => {
 		if (!loading) {
 			console.log("fetch loading done");
 			console.log(gameList);
-			getSortedGameList(10);
+			getSortedGameList(12);
 		}
 	}, [loading]);
+
+	useEffect(() => {
+		if (!subLoad) {
+			console.log("subload done");
+			console.log(sortedGameList);
+		}
+	}, [subLoad]);
 
 	const sortedGames = gameList.sort((game1, game2) =>
 		game1.metacriticScore < game2.metacriticScore
@@ -36,22 +44,17 @@ const ContentContainer = () => {
 
 	function getSortedGameList(n, arr = sortedGames) {
 		for (let i = 0; i < n; i++) {
-			// tmpArr.push(arr[i]);
 			setSortedGameList((prevSorted) => [...prevSorted, arr[i]]);
 		}
 		setSubLoad(!subLoad);
 	}
 
-	useEffect(() => {
-		if (!subLoad) {
-			console.log("subload done");
-			console.log(sortedGameList);
-		}
-	}, [subLoad]);
-
 	const DISPLAY_GAME_LIST = sortedGameList.map((game) => {
 		return <li key={game.steamAppID}> {game.title}</li>;
 	});
-	return <>{subLoad ? <>LOADING CONTENTS...</> : DISPLAY_GAME_LIST}</>;
+	return (
+		<div className={`${contentStyles.gridContainer}`}>{subLoad ? <>LOADING CONTENTS...</> : DISPLAY_GAME_LIST}</div>
+
+	);
 };
 export default ContentContainer;

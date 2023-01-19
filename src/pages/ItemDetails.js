59e75958f70437a66a73db5bc20710084b60ec88
Fdiaz <jerylfranz460@gmail.com>
2023-01-20 03:06:39 +0400

    fetching giphy api and passing the state ti item detail component

--- src/pages/ItemDetails.js
+++ src/pages/ItemDetails.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ItemDisplay from "../components/Items/ItemDisplay";
-import "../components/Items/ItemCSS/itemDetails.css"
+import "../components/Items/ItemCSS/itemDetails.css";
 const ItemDetails = () => {
 	const { id } = useParams();
 	const [gameTitle, setGameTitle] = useState({});
@@ -10,19 +10,33 @@ const ItemDetails = () => {
 	async function fetchGameDetails() {
 		const fetchedGameApi = await fetch(`https://barter.vg/steam/app/${id}/json`);
 		const fetchedGame = await fetchedGameApi.json();
-		setGameTitle(fetchedGame);
+		const fetchedGametitle = fetchedGame.title_formatted;
+
+		const fetchGiphyApi = await fetch(
+			`https://api.giphy.com/v1/gifs/search?api_key=6C6Bv7RmIvF06uPUz6RVMaQgiWxSQiKd&q=${fetchedGametitle}-game&limit=1&offset=0&rating=pg-13&lang=en`
+		);
+		const fetchedGif = await fetchGiphyApi.json();
+		const gifURL = fetchedGif.data[0].url;
+
+		setGameTitle({ ...fetchedGame, gifUrl: gifURL });
+
 		setLoading(!loading);
-		console.log(fetchedGame);
 	}
 
 	useEffect(() => {
 		fetchGameDetails();
 	}, []);
 
+	useEffect(() => {
+		if (!loading) {
+			console.log("loading done");
+            console.log(gameTitle)
+		}
+	}, [loading]);
 	return (
-        // pass the fetched data from api to item display as props
+		// pass the fetched data from api to item display as props
 		<div className="item-detail-main-container">
-			<ItemDisplay />
+			{loading ? <>LOADING ITEM DETAILS...</> : <ItemDisplay gameDetails={gameTitle} />}
 		</div>
 	);
 };

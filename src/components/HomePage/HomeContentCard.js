ad3f73018a1cc60de4e958a0653a677165f678fe
Fdiaz <jerylfranz460@gmail.com>
2023-01-19 20:31:13 +0400

    unable to fetch third party api wthrough client side http request due to cors policy

--- src/components/HomePage/HomeContentCard.js
+++ src/components/HomePage/HomeContentCard.js
@@ -1,6 +1,9 @@
-import React from "react";
-import contentCardStyles from "./ContentCard.module.css"
+import React, { useEffect } from "react";
+import contentCardStyles from "./ContentCard.module.css";
 const HomeContentCard = ({ gameData }) => {
+
+
+
 	return (
 		<li id={`${gameData.internalName}-list`} className={`${contentCardStyles.cardList}`}>
 			<div id="home-card-container" className={`${contentCardStyles.homeCardContainer}`}>
@@ -14,10 +17,10 @@ const HomeContentCard = ({ gameData }) => {
 						<h4>Metacritic Score:</h4>
 					</div>
 					<div id="home-card-priceNcritics">
-                        <h4>${gameData.salePrice}</h4>
-                        <h4>{gameData.steamRatingPercent}%</h4>
-                        <h4>{gameData.metacriticScore}</h4>
-                    </div>
+						<h4>${gameData.salePrice}</h4>
+						<h4>{gameData.steamRatingPercent}%</h4>
+						<h4>{gameData.metacriticScore}</h4>
+					</div>
 				</div>
 			</div>
 		</li>

4502c0e4f61a9355710875124cf22927dfe3ed7e
Fdiaz <jerylfranz460@gmail.com>
2023-01-19 20:42:35 +0400

    lining up the titles and contents of a home card

--- src/components/HomePage/HomeContentCard.js
+++ src/components/HomePage/HomeContentCard.js
@@ -1,9 +1,6 @@
 import React, { useEffect } from "react";
 import contentCardStyles from "./ContentCard.module.css";
 const HomeContentCard = ({ gameData }) => {
-
-
-
 	return (
 		<li id={`${gameData.internalName}-list`} className={`${contentCardStyles.cardList}`}>
 			<div id="home-card-container" className={`${contentCardStyles.homeCardContainer}`}>
@@ -11,15 +8,19 @@ const HomeContentCard = ({ gameData }) => {
 					<img src={gameData.thumb} className={`${contentCardStyles.homeCardImage}`} />
 				</div>
 				<div id="home-card-textNprice-container" className={`${contentCardStyles.textNpriceDisplay}`}>
-					<div id="home-card-textNtitle">
-						<h3>{gameData.title}</h3>
-						<h4>Steam Rating: </h4>
-						<h4>Metacritic Score:</h4>
-					</div>
-					<div id="home-card-priceNcritics">
-						<h4>${gameData.salePrice}</h4>
-						<h4>{gameData.steamRatingPercent}%</h4>
-						<h4>{gameData.metacriticScore}</h4>
+					<div id="home-card-textNtitle" className="home-card-contents">
+						<div className="home-cards" id="home-card-title">
+							<h3>{gameData.title}</h3>
+							<h4 className="home-card-title-score-ratings">${gameData.salePrice}</h4>
+						</div>
+						<div className="home-cards" id="home-card-steam-ratings">
+							<h4>Steam Rating: </h4>
+							<h4 className="home-card-title-score-ratings">{gameData.steamRatingPercent}%</h4>
+						</div>
+						<div className="home-cards" id="home-card-score">
+							<h4>Metacritic Score:</h4>
+							<h4 className="home-card-title-score-ratings">{gameData.metacriticScore}</h4>
+						</div>
 					</div>
 				</div>
 			</div>

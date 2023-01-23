3ee1f5fea67358811e41e8bc9f7cb93a35e64fcd
zaid-lp <jerylfranz460@gmail.com>
2023-01-23 05:44:50 +0400

    FEAT: add function to calculate percentage of price, used use effect to wait for all of the asynchronous task to finish and then invoke the function

--- src/components/Items/ItemDisplay.js
+++ src/components/Items/ItemDisplay.js
@@ -41,8 +41,18 @@ const ItemDisplay = ({ gameDetails }) => {
 				totalReviews: obj.user_reviews_total,
 				userRating: obj.user_reviews_positive,
 				price: obj.price / 100,
+				beforePrice: obj.price_high / 100,
 			};
 		});
+	}
+
+	function getDiscountPercentage({ price, beforePrice }) {
+		console.log(price);
+		const diff = price - beforePrice;
+		const ave = (price + beforePrice) / 2;
+		const discount = (diff / ave) * 100;
+		console.log(diff, ave, discount);
+		return setGameSpecs((prevState) => ({ ...prevState, discount: discount }));
 	}
 	useEffect(() => {
 		Promise.allSettled([
@@ -51,10 +61,14 @@ const ItemDisplay = ({ gameDetails }) => {
 			getDeveloper(gameDetails.credits),
 		]).then((data) => {
 			setLoading(!loading);
-			console.log(gameSpecs);
-			console.log(gameDetails);
 		});
 	}, []);
+
+	useEffect(() => {
+		if (!loading) {
+			getDiscountPercentage(gameSpecs);
+		}
+	}, [loading]);
 
 	const displayGenres =
 		!loading &&
@@ -80,10 +94,12 @@ const ItemDisplay = ({ gameDetails }) => {
 	const displayPrices = !loading ? (
 		<ul className="item-price-information-list">
 			<li>Price: ${gameSpecs.price}</li>
+			<li>Before: ${gameSpecs.beforePrice}</li>
 		</ul>
 	) : null;
 	return (
 		<div className="item-display-container">
+			<span className="item-floating-sale">{gameSpecs.discount}</span>
 			<div className="item-display-img-container">
 				<img src={gameDetails.gifUrl} alt="game-image" />
 			</div>
@@ -96,9 +112,7 @@ const ItemDisplay = ({ gameDetails }) => {
 
 					{/* redirect to steam page if site is empty */}
 				</div>
-				<div className="item-pricing-container">
-					{displayPrices}
-				</div>
+				<div className="item-pricing-container">{displayPrices}</div>
 				<div className="item-ratings">
 					<div className="item-rating item-steam-rating">
 						<h4 className="item-rating-score">

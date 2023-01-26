ff8569e606e0eed37d01cd006e78a696663e147e
zaid-lp <jerylfranz460@gmail.com>
2023-01-26 11:07:18 +0400

    REF using context to prevent prop drilling

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 import React from "react";
-import RouterSwitch  from "./pages/RouterSwitch";
+import RouterSwitch from "./pages/RouterSwitch";
+import React, { createContext, useState } from "react";
+
+export const shoppingContext = createContext();
 
 const App = () => {
-	return <RouterSwitch />;
+
+	const [shoppingCart, setShoppingCart] = useState([]);
+	function addToCart(item) {
+        console.log(item, shoppingCart)
+		return setShoppingCart((prevState) => [...prevState, item]);
+	}
+
+	return (
+		<shoppingContext.Provider value={{ shoppingCart, addToCart }}>
+			<RouterSwitch />
+		</shoppingContext.Provider>
+	);
 };
 
 export default App;

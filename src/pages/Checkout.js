69797c292c4684cf30c48e9c36e537d84a93a971
francis <jerylfranz460@gmail.com>
2023-01-27 04:15:37 +0400

    REF: addddd

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useCallback, useContext, useEffect } from "react";
+import { shoppingContext } from "../app";
 import CheckoutItems from "../components/Checkout/CheckoutItems";
 
-
 const Checkout = () => {
+
+    const { shoppingCart, _ } = useContext(shoppingContext)
+    // https://www.cheapshark.com/api/1.0/games?steamAppID= 
+    useEffect(() => {
+
+    }, [])
+
     return (
         <div>
             <CheckoutItems />

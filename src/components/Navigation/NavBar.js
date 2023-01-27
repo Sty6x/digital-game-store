f20ff144ad0563aca770611b435fda3fe97c3805
francis <jerylfranz460@gmail.com>
2023-01-27 08:09:44 +0400

    STYLE: made function to replace sidebar activity if active and if not

--- src/components/Navigation/NavBar.js
+++ src/components/Navigation/NavBar.js
@@ -1,9 +1,9 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import { Link } from "react-router-dom";
 import { shoppingContext } from "../../app";
 import navStyles from "./Navigation.module.css";
 const NavBar = () => {
-
+	const sideBarRef = useRef(null)
 	const { shoppingCart, _ } = useContext(shoppingContext);
 	const pageLinks = [
 		{ path: "", url: "Home" },
@@ -18,6 +18,17 @@ const NavBar = () => {
 			</li>
 		);
 	});
+
+	function sideBarActivity(ref) {
+		if (ref.current.classList.contains(navStyles.checkoutSidebarActive)) {
+			ref.current.classList.replace(`${navStyles.checkoutSidebarActive}`, `${navStyles.chechoutSidebarInactive}`)
+		} else {
+
+			ref.current.classList.replace(`${navStyles.chechoutSidebarInactive}`, `${navStyles.checkoutSidebarActive}`)
+		}
+		console.log(ref.current)
+	}
+
 	return (
 
 		<div className={`nav-container ${navStyles.size} ${navStyles.navContDisplay}`}>
@@ -25,10 +36,12 @@ const NavBar = () => {
 				<h1 className={`logo ${navStyles.logoFont}`}><Link to={'/'}>GAME DEALS</Link></h1>
 				<ul className={`nav-links ${navStyles.linksGap} ${navStyles.linksMargin} ${navStyles.linksFont} ${navStyles.linksDisplay}`}>{DISPLAY_LINKS}
 					{shoppingCart.length !== 0 && <span className={navStyles.floatItemsCart}>{shoppingCart.length}</span>}
-					<button className={navStyles.checkoutBtn} >CHECKOUT</button>
+					<button onClick={() => {
+						sideBarActivity(sideBarRef)
+					}} className={navStyles.checkoutBtn} >CHECKOUT</button>
 				</ul>
 			</div>
-			<div className={navStyles.checkoutSidebar}>a</div>
+			<div ref={sideBarRef} className={navStyles.checkoutSidebarActive}></div>
 		</div>
 	);
 };

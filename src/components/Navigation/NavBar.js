6e481cc8266f3c3f87b75ccc4cd4a904222bce9f
francis <jerylfranz460@gmail.com>
2023-01-27 04:01:00 +0400

    STYLE: add shopping cart item count so users would be able to see the amount of items they currently have

--- src/components/Navigation/NavBar.js
+++ src/components/Navigation/NavBar.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { shoppingContext } from "../../app";
 import navStyles from "./Navigation.module.css";
 const NavBar = () => {
+
+	const { shoppingCart, _ } = useContext(shoppingContext);
 	const pageLinks = [
 		{ path: "", url: "Home" },
 		{ path: "shop", url: "Shop" },
 		{ path: "checkout", url: "Checkout" },
 	];
+
 	const DISPLAY_LINKS = pageLinks.map((pageLink) => {
 		return (
 			<li className="links" key={pageLink.url}>
@@ -17,7 +21,9 @@ const NavBar = () => {
 	return (
 		<div className={`nav-container ${navStyles.size} ${navStyles.navContDisplay}`}>
 			<h1 className={`logo ${navStyles.logoFont}`}><Link to={'/'}>GAME DEALS</Link></h1>
-			<ul className={`nav-links ${navStyles.linksGap} ${navStyles.linksMargin} ${navStyles.linksFont} ${navStyles.linksDisplay}`}>{DISPLAY_LINKS}</ul>
+			<ul className={`nav-links ${navStyles.linksGap} ${navStyles.linksMargin} ${navStyles.linksFont} ${navStyles.linksDisplay}`}>{DISPLAY_LINKS}
+				{shoppingCart.length !== 0 && <span className={navStyles.floatItemsCart}>{shoppingCart.length}</span>}
+			</ul>
 		</div>
 	);
 };

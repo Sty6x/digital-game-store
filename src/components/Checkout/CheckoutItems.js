6e99a94ee4247af9a50c3e84ff9e5b903bdadf4c
francis <jerylfranz460@gmail.com>
2023-01-27 07:17:44 +0400

    REF:add keys to each component

---
+++ src/components/Checkout/CheckoutItems.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const CheckoutItems = ({ gameData }) => {
+    console.log(gameData)
+    return (<> ITEM </>)
+}
+
+export default CheckoutItems
--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,11 +1,37 @@
 import React from "react";
 
 
-const Checkout = ( ) =>{
-    return(
+const Checkout = () => {
+    const { shoppingCart, _ } = useContext(shoppingContext)
+    const [shoppingCartItems, setShoppingCartItems] = useState([]);
+    const [loading, setLoading] = useState(true);
+    // https://www.cheapshark.com/api/1.0/games?steamAppID= 
+
+    function fetchGameList(cart) {
+        return Promise.all(cart.map(async item => {
+            const fetchAPI = await fetch(`https://www.cheapshark.com/api/1.0/games?steamAppID=${item}`)
+            const data = await fetchAPI.json()
+            return data[0]
+        }))
+    }
+    useEffect(() => {
+        fetchGameList(shoppingCart).then(responses => {
+            console.log(responses)
+            return responses
+        }).then(parsedJsonData => {
+            setShoppingCartItems(prev => [...prev, ...parsedJsonData])
+        }).then(_ => {
+            setLoading(!loading)
+        })
+    }, [])
+
+    const displayCheckoutItems = shoppingCartItems.map(item => {
+        return (<CheckoutItems key={item.steamAppID} gameData={item} />)
+    })
+    return (
         <div>
             Checkout
-            </div>
+        </div>
     )
 }
 

0ec928170100920dc176653b230a4dc574e18c14
francis <jerylfranz460@gmail.com>
2023-01-27 06:12:30 +0400

    REF: able to fetch data steam app id provided from parent component to grab data from shark api and make every fetched data be done at the same time

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,18 +1,34 @@
-import React, { useCallback, useContext, useEffect } from "react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
 import { shoppingContext } from "../app";
 import CheckoutItems from "../components/Checkout/CheckoutItems";
 
 const Checkout = () => {
 
     const { shoppingCart, _ } = useContext(shoppingContext)
+    const [shoppingCartItems, setShoppingCartItems] = useState([]);
+    const [loading, setLoading] = useState(true);
     // https://www.cheapshark.com/api/1.0/games?steamAppID= 
-    useEffect(() => {
 
+    function fetchGameList(cart) {
+        return cart.map(item => {
+            return fetch(`https://www.cheapshark.com/api/1.0/games?steamAppID=${item}`)
+        })
+    }
+    useEffect(() => {
+        Promise.all(fetchGameList(shoppingCart))
+            .then(reponses => {
+                reponses.map(res => res.json())
+            }).then(parsedJsonData => {
+                return setShoppingCartItems(prev => [...prev, parsedJsonData])
+            }).then(_ => setLoading(!loading))
+            .catch(err => {
+                throw err
+            })
     }, [])
 
     return (
         <div>
-            <CheckoutItems />
+            {!loading && <CheckoutItems gameData={shoppingCartItems} />}
         </div>
     )
 }

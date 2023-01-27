0624bca118231624455aed9b37d250d4ac3ecfed
francis <jerylfranz460@gmail.com>
2023-01-27 22:36:04 +0400

    STYLE: added padding on sidebar container

--- src/components/Navigation/Checkout/CheckoutSidebar.js
+++ src/components/Navigation/Checkout/CheckoutSidebar.js
@@ -3,16 +3,17 @@ import { shoppingContext } from "../../../app";
 import CheckoutItems from "./CheckoutItems";
 
 const CheckoutSidebar = ({ sideBarRef, shoppingCartItems }) => {
+    const { shoppingCart, _ } = useContext(shoppingContext)
 
     // https://www.cheapshark.com/api/1.0/games?steamAppID= 
 
-    const displayCheckoutItems = shoppingCartItems.map(item => {
+    const displayCheckoutItems = (shoppingCart.length !== 0) ? shoppingCartItems.map(item => {
         return (<CheckoutItems key={item.steamAppID} gameData={item} />)
-    })
+    }) : <>Shopping cart is empty</>
 
 
     return (
-        <div ref={sideBarRef} className="checkout-sidebar">
+        <div ref={sideBarRef} className="checkout-sidebar-inactive checkout-sidebar">
             <div className="checkout-contents-container">
                 {displayCheckoutItems}
             </div>

05fd0c8e58feeb6c5959054251e66afb47a212ea
francis <jerylfranz460@gmail.com>
2023-01-29 12:29:36 +0400

    STYLE: add fonts

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,12 +2,11 @@ import { Link } from "react-router-dom";
 import "../components/HomePage/Homepage.css";
 import image from "../assets/img/flatHomeImg(1).jpg"
 function HomePage() {
-	// Refactor: Let Homepage component fetch apis and pass result as props to contentContainer instead.
 	return (
 		<div className="hpContainer">
 			<div className="home-introduction-section">
 				<p className="home-intro-p">Game Deals is the best place to find the best discounts on the most popular video games.</p>
-				<button type="button"><Link to="/shop" >Shop now</Link></button>
+				<button className="home-toshop-btn" type="button"><Link to="/shop" >Shop now</Link></button>
 			</div>
 			<div className="home-img-section">
 				<div className="home-img-container">

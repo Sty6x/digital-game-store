d761a190e08106db30064a97e07ce695740ea13e
francis <jerylfranz460@gmail.com>
2023-01-29 09:27:52 +0400

    REF: changing setting if body has the homepage container nad if so hide scroll bar

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,8 @@
 import homeStyles from "../components/HomePage/HomePage.module.css";
-import ContentContainer from "../components/HomePage/ContentContainer";
 function HomePage() {
 	// Refactor: Let Homepage component fetch apis and pass result as props to contentContainer instead.
 	return (
 		<div className={`hpContainer ${homeStyles.size} ${homeStyles.display} ${homeStyles.spacing}`}>
-			<ContentContainer />
 		</div>
 	);
 }

7703795f3bec4e2def7fdfd0495e2a5ef46ebd44
francis <jerylfranz460@gmail.com>
2023-01-30 10:02:17 +0400

    STYLE: rudimentary styling

--- src/components/Navigation/Checkout/CheckoutItems.js
+++ src/components/Navigation/Checkout/CheckoutItems.js
@@ -2,21 +2,23 @@ import React from "react";
 import "./checkoutItems.css"
 
 const CheckoutItems = ({ gameData }) => {
-
-    return (<div className="checkout-item-container">
-        <div className="checkout-item-img-section">
-            <div className="checkout-item-img-container">
-                <img className="checkout-item-img" alt="checkout item" />
+    console.log(gameData)
+    return (
+        <div className="checkout-item-container">
+            <div className="checkout-item-img-section">
+                <div className="checkout-item-img-container">
+                    <img className="checkout-item-img" alt="checkout item" src={gameData.thumb} />
+                </div>
             </div>
-        </div>
-        <div className="checkout-item-descAndoptions-section">
-            <h2 className="checkout-item-title">Dawn of War</h2>
-            <button >delete</button>
-        </div>
-        <div className="checkout-item-price-section">
-
-        </div>
-    </div>)
+            <div className="checkout-item-descAndoptions-section">
+                <h2 className="checkout-item-title">{gameData.external}</h2>
+                <button className="delete-item">delete</button>
+            </div>
+            <div className="checkout-item-price-section">
+                <p>Price:</p>
+                {gameData.cheapest}
+            </div>
+        </div>)
 }
 
 export default CheckoutItems

7405b881e22aa08f32bbd2577a48b0933ae25f95
francis <jerylfranz460@gmail.com>
2023-01-30 09:49:01 +0400

    STYLE: add structure to checkout items

--- src/components/Navigation/Checkout/CheckoutItems.js
+++ src/components/Navigation/Checkout/CheckoutItems.js
@@ -3,8 +3,19 @@ import "./checkoutItems.css"
 
 const CheckoutItems = ({ gameData }) => {
 
-    return (<div className="checkout-items-container">
-        {gameData.external}
+    return (<div className="checkout-item-container">
+        <div className="checkout-item-img-section">
+            <div className="checkout-item-img-container">
+                <img className="checkout-item-img" alt="checkout item" />
+            </div>
+        </div>
+        <div className="checkout-item-descAndoptions-section">
+            <h2 className="checkout-item-title">Dawn of War</h2>
+            <button >delete</button>
+        </div>
+        <div className="checkout-item-price-section">
+
+        </div>
     </div>)
 }
 
